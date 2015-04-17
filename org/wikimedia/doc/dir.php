902a752f4188af9304bff920943fc9115b4f055c
Timo Tijhof <krinklemail@gmail.com>
2015-04-17 20:20:02 +0100

    doc: Enable pretty directory index for doc.wikimedia.org
    Follows-up 486fec7, which enabled it for https://integration.wikimedia.org/cover.
    
    This will turn pages like https://doc.wikimedia.org/mediawiki-core/ into
    normal page list instead of Apache's default directory index.
    
    Keeps navigation consistent with the rest of this domain.
    
    Change-Id: I9f865c1b2bcd3080db227e7c942f12ff2084406f

---
+++ org/wikimedia/doc/dir.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/../../../shared/DocPage.php';
+
+$p = DocPage::newDirIndex( '', Page::INDEX_PREFIX | Page::INDEX_PARENT_PREFIX | Page::INDEX_ALLOW_SKIP );
+$p->setRootDir( __DIR__ );
+$p->handleDirIndex();
