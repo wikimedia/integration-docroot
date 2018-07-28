wikimedia/integration-docroot

commit 9f168878e0a5853aa2addc489e2da7abb932ec38

CoveragePage: Add test cases for parseClover()
Change-Id: I379a00e60d2774725c69b1952dd3cbfa8609db93

shared/CoveragePage.php
@@ -44,7 +44,7 @@ class CoveragePage extends DocPage {
 	 */
 	public function handleCoverageIndex() {
 		// Get list of directories with clover.xml
-		$results = glob( $this->coverageDir . '/*/clover.xml' );
+		$cloverFiles = glob( $this->coverageDir . '/*/clover.xml' );
 		$this->embedCSS( file_get_contents( __DIR__ . '/cover.css' ) );
 
 		$sort = isset( $_GET['sort'] ) ? (string)$_GET['sort'] : null;
@@ -102,7 +102,12 @@ HTML;
 		$this->addHtmlContent( '<ul class="nav nav-pills nav-stacked cover-list">' );
 		$html = '';
 		$clovers = [];
-		foreach ( $results as $clover ) {
+		foreach ( $cloverFiles as $cloverFile ) {
+			$clover = file_get_contents( $cloverFile );
+			if ( !$clover ) {
+				// Race condition?
+				continue;
+			}
 			$clovers[$clover] = $this->parseClover( $clover );
 		}
 		if ( isset( $_GET['sort'] ) && $_GET['sort'] === 'cov' ) {
@@ -139,16 +144,10 @@ HTML;
 	/**
 	 * Get data out of the clover.xml file
 	 *
-	 * @param string $fname
+	 * @param string $contents Contents of a clover.xml file
 	 * @return array|bool false on failure
 	 */
-	protected function parseClover( $fname ) {
-		$contents = file_get_contents( $fname );
-		if ( !$contents ) {
-			// Race condition?
-			return false;
-		}
-
+	protected function parseClover( $contents ) {
 		$types = [ 'methods', 'conditionals', 'statements', 'elements' ];
 		$total = 0;
 		$xml = new SimpleXMLElement( $contents );
@@ -177,7 +176,7 @@ HTML;
 		}
 		// TODO: Figure out how to get a more friendly name
 		return [
-			'percent' => $percent * 100,
+			'percent' => round( $percent * 100, 2 )
 		];
 	}
 

shared/test/CoveragePageTest.php
@@ -1,0 +1,71 @@
+<?php
+require_once dirname( __DIR__ ) . '/CoveragePage.php';
+
+/**
+ * @covers CoveragePage
+ */
+class CoveragePageTest extends PHPUnit\Framework\TestCase {
+
+	public static function provideParseClover() {
+		return [
+			'Empty file' => [
+				'',
+				false,
+			],
+			'Missing project root' => [
+				'<example></example>',
+				[ 'percent' => 100.0 ],
+			],
+			'Sample file' => [
+				// From wikimedia/wrappedstring
+				'<?xml version="1.0" encoding="UTF-8"?>'
+					. '<coverage generated="1"><project timestamp="2">'
+					. '<metrics files="2" loc="232" ncloc="107" classes="2" methods="10" coveredmethods="8" conditionals="0" coveredconditionals="0" statements="52" coveredstatements="48" elements="62" coveredelements="56"/>'
+					. '</project></coverage>',
+				[ 'percent' => 90.32 ]
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider provideParseClover
+	 */
+	public function testParseClover( $input, $expected ) {
+		$class = new ReflectionClass( CoveragePage::class );
+		$method = $class->getMethod( 'parseClover' );
+		$method->setAccessible( true );
+
+		$page = CoveragePage::newFromPageName( 'Example' );
+
+		if ( $expected === false ) {
+			$this->setExpectedException( Exception::class );
+		}
+
+		$this->assertSame(
+			$expected,
+			$method->invokeArgs( $page, [ $input ] )
+		);
+	}
+
+	/**
+	 * @see PHPUnit\Framework\TestCase::setExpectedException
+	 *
+	 * Compatibility with PHPUnit 4 and PHPUnit 6,
+	 * which renamed setExpectedException() to expectException().
+	 */
+	public function setExpectedException( $name, $message = '', $code = null ) {
+		if ( is_callable( [ $this, 'expectException' ] ) ) {
+			if ( $name !== null ) {
+				$this->expectException( $name );
+			}
+			if ( $message !== '' ) {
+				$this->expectExceptionMessage( $message );
+			}
+			if ( $code !== null ) {
+				$this->expectExceptionCode( $code );
+			}
+		} else {
+			parent::setExpectedException( $name, $message, $code );
+		}
+	}
+}

commit 55cb62bde84b2509b817693b33ae1059fb02dff7

cover: Fix variable reuse
Regression from 9f168878e0a.

Change-Id: Ie82091516ec4d01fd2ca81d4987c5aa2d6945040

shared/CoveragePage.php
@@ -108,7 +108,7 @@ HTML;
 				// Race condition?
 				continue;
 			}
-			$clovers[$clover] = $this->parseClover( $clover );
+			$clovers[$cloverFile] = $this->parseClover( $clover );
 		}
 		if ( isset( $_GET['sort'] ) && $_GET['sort'] === 'cov' ) {
 			// Order by coverage, ascending
@@ -119,8 +119,8 @@ HTML;
 				return ( $a['percent'] < $b['percent'] ) ? -1 : 1;
 			} );
 		}
-		foreach ( $clovers as $clover => $info ) {
-			$dirName = htmlspecialchars( basename( dirname( $clover ) ) );
+		foreach ( $clovers as $cloverFile => $info ) {
+			$dirName = htmlspecialchars( basename( dirname( $cloverFile ) ) );
 			$percent = (string)round( $info['percent'] );
 			$color = $this->getLevelColor( $info['percent'] );
 			$minWidth = $percent >= 10 ? '3em' : '2em';