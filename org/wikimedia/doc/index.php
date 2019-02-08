6c02225a9093b90c0a4b349b4914d923fb1cf213
Timo Tijhof <krinklemail@gmail.com>
2019-02-08 00:16:10 +0000

    doc: Multi-column magic
    Change-Id: I961f16f0de91d088837f5fe5c29b4930f32b05ff

--- org/wikimedia/doc/index.php
+++ org/wikimedia/doc/index.php
@@ -5,6 +5,15 @@ $p = DocPage::newIndex();
 $p->setRootDir( __DIR__ );
 
 $p->setDir( __DIR__ );
+$p->embedCSS( '
+	.wm-docindex-list {
+		column-width: 25em;
+	}
+	.wm-docindex-list section {
+		min-width: 20em;
+		break-inside: avoid;
+	}
+' );
 $p->addHtmlFile( 'default.html' );
 
 $p->flush();
