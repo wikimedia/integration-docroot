920d17c9ea74655cd338e92d1647147c367dc609
Timo Tijhof <krinklemail@gmail.com>
2019-02-08 00:34:07 +0000

    doc: Even out un-even margins in columns
    Follows 6c02225a9093b90. Common css trick, but forgot about this
    initially.
    
    Change-Id: Ic8c1f94504662c9e8e81ba5588b4eb1d369ca8fd

--- org/wikimedia/doc/index.php
+++ org/wikimedia/doc/index.php
@@ -12,6 +12,11 @@ $p->embedCSS( '
 	.wm-docindex-list section {
 		min-width: 20em;
 		break-inside: avoid;
+		/* Ensure margin is contained so that each column starts similarly.
+		   Without this, the margin of the first child of each section
+		   (e.g. heading) would be transferred to the bottom of a prior
+		   column, thus causing later columns to lack their top margin. */
+		display: inline-block;
 	}
 ' );
 $p->addHtmlFile( 'default.html' );
