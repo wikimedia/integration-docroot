cd32400be2ed458a61f6693a394490a3b3e59290
Timo Tijhof <krinklemail@gmail.com>
2020-04-25 16:18:02 +0100

    doc.wikimedia.org: Remove unused dir.php copies
    The apache configuration for doc.wm.o always rewrites to the main
    dir.php file, which suffices. These copies were from the time that
    this directory was still mounted under integration.wikimedia.org.
    
    Change-Id: I9ff3cd8914c26296cc3568a9c17bdf65aa3f4acb

--- org/wikimedia/doc/cover/dir.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../../../../shared/DocPage.php';
-
-$p = DocPage::newDirIndex( 'Coverage: ', Page::INDEX_PREFIX | Page::INDEX_ALLOW_SKIP );
-$p->setRootDir( dirname( __DIR__ ) );
-$p->handleDirIndex();
-$p->flush();
