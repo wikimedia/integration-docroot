2aeb9c1ad8fdb8bad83c7f6cee35744d75cb08ed
Antoine Musso <hashar@free.fr>
2022-05-18 19:24:10 +0200

    build: override mediawiki-phan-config directory lists
    I have MW_INSTALL_PATH environment variable set locally which is
    recognized by mediawiki/mediawiki-phan-config and cause the Phan
    analyzis to fail.
    
    In .phan/config.php override rather than expand `directory_list` and
    `exclude_analysis_directory_list`.
    
    Bug: T308693
    Change-Id: I538d8b324593a912963d0d1ff2cb394f42b96da0

--- .phan/config.php
+++ .phan/config.php
@@ -1,7 +1,15 @@
 <?php
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
-$cfg['directory_list'][] = dirname( __DIR__ ) . '/dev/';
-$cfg['directory_list'][] = dirname( __DIR__ ) . '/org/';
-$cfg['directory_list'][] = dirname( __DIR__ ) . '/shared/';
-$cfg['exclude_analysis_directory_list'][] = dirname( __DIR__ ) . '/shared/test/';
+
+// We override the directory lists to erase the defaults provided by
+// mediawiki-phan-config which are specific to MediaWiki.
+$cfg['directory_list'] = [
+	dirname( __DIR__ ) . '/dev/',
+	dirname( __DIR__ ) . '/org/',
+	dirname( __DIR__ ) . '/shared/',
+];
+$cfg['exclude_analysis_directory_list'] = [
+	dirname( __DIR__ ) . '/shared/test/',
+];
+
 return $cfg;
